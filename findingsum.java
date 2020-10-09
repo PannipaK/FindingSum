fa6cddb19991d541bdb1321428690403666dfdb2
Pannipa K <55374984+PannipaK@users.noreply.github.com>
2020-10-09 10:57:55 -0700

    Add files via upload

---
+++ findingsum.java
@@ -0,0 +1,14 @@
+public class findingsum{
+    public findingsum(){
+
+    }
+    public static void main(String[]args){
+        double [] dArray = { 9.5, 12.1, 8.7 };
+        for(int i=0; i<dArray.length; i++){
+            System.out.println(dArray[i]);
+            //System.out.println(dArray[0]);
+            //System.out.println(dArray[1]);
+            //System.out.println(dArray[2]);
+        }
+    }
+}
